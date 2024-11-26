16d26d91c9a0760e49117eb9810765d589f24147
Bjørnar Dervo <76587915+DervoDev@users.noreply.github.com>
2024-11-26 14:01:54 +0100

    Update norwayTest.sh

--- norwayTest.sh
+++ norwayTest.sh
@@ -8,7 +8,7 @@ echo "====> : Run download area "
 
 #make download-geofabrik area=norway
 #make destroy-db
-#make stop-db
+make stop-db
 #make clean-unnecessary-docker
 #make remove-docker-images
 #make refresh-docker-images
